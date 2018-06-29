8015e3a6521bcdf18f99318aeb8bb4c739f15e2c
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-06-30 03:32:04 +0700

    mm

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <darshit/>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to gajjar darshit</h1>
+        </header>
+        <p className="App-intro">
+          cism <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>gajjar darshit</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
